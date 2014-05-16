044a9d61f259441c653978ac5c45cfa6750bf40c
TerminalTesting <testing@terminal.ru>
2014-05-16 13:07:58 +0600

    upload test files

---
+++ tsettings.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+import unittest
+import sys
+import os
+import paramiko
+
+
+class ChangePref(unittest.TestCase):
+        
+    def tearDown(self):
+        """Удаление переменных для всех тестов. Остановка приложения"""
+
+        if sys.exc_info()[0]:   
+            print sys.exc_info()[0]
+
+
+    
+
+    def test_changer(self):
+
+        cnt=0 #счетчик ошибок теста
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(os.getenv('HOST'),username=os.getenv('USERNAME'),password=os.getenv('PASS'))
+            ssh.exec_command('sudo cp /home/tdevel/%s/operaprefs.ini /home/terminal/.opera' % os.getenv('CHOISE'))
+            ssh.exec_command('sudo cp /home/tdevel/%s/script.js /home/terminal/.opera/userjs' % os.getenv('CHOISE'))
+            ssh.exec_command('sudo killall opera')
+            ssh.close()
+            print 'Files successfully changed'
+        except:
+            cnt=1
+            print 'Error occurred!!!'
+       
+        assert cnt==0, (u'Error!!!')
